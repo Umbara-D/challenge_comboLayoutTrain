8bc9142448a855c4428e7a0d15c3fddbb16d582b
haidarrahman30 <haidarrahmans30@gmail.com>
2021-03-05 15:26:45 +0800

    Push project challenge Layout Android-App

---
+++ app/src/main/java/com/example/challenge/ChallengeSatu.kt
@@ -0,0 +1,11 @@
+package com.example.challenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChallengeSatu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_challenge_satu)
+    }
+}
